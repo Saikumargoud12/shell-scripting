48b2f0230989a36714de8568c8aebad8505f99d9
saikumargoud12 <sabbagauni@gmail.com>
2022-09-16 18:13:14 -0400

    she-bang basics

--- shebang.sh
+++ shebang.sh
@@ -1,3 +1,4 @@
 # #! is called as she-bang
 # completly optional
 # adding that will have an understanding what script it is
+# shell scripting is nothing but adding all your manual commands in order
