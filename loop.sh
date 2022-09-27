bcd284551127800e75a489d565613e6230018447
saikumargoud12 <sabbagauni@gmail.com>
2022-09-26 21:42:00 -0400

    if conditions

--- loop.sh
+++ loop.sh
@@ -1,19 +1,21 @@
 # there are two basic loops
 # while and for loop
-if we need to execute to same commands again and again we use loops.
+#if we need to execute to same commands again and again we use loops.
 
 #while loop
 
 a=10
 
-while[$a -gt 0]; do
+while[$a -gt 0];do
   echo hello -$a
   a=$(($a-1))
+  sleep 1
   done
 
 #loop conditions executes until the expression fails
- for fruit in apple banana orange; do
+ for fruit in apple banana orange;do
    echo fruit name =$fruit
+   sleep 1
 done
 
 
