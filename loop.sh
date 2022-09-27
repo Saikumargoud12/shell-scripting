303b33af845bc67050f9b0c86c4a93ae72bd2d8d
saikumargoud12 <sabbagauni@gmail.com>
2022-09-26 21:44:38 -0400

    if conditions

--- loop.sh
+++ loop.sh
@@ -6,7 +6,7 @@
 
 a=10
 
-while [$a -gt 0]; do
+while [ $a -gt 0 ]; do
   echo hello - $a
   a=$(($a-1))
   sleep 1
@@ -14,7 +14,7 @@ while [$a -gt 0]; do
 
 #loop conditions executes until the expression fails
  for fruit in apple banana orange; do
-   echo fruit name =$fruit
+   echo fruit name = $fruit
    sleep 1
 done
 
